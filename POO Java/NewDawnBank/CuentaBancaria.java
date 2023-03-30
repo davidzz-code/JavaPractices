aba527ef15a391ce5c514ac7cbb7db3bd3024368
David Ramírez <rrdavid08@gmail.com>
2023-03-30 17:24:44 +0200

    Comprobar IBAN y mostrar movimientos arreglado

--- POO Java/NewDawnBank/CuentaBancaria.java
+++ POO Java/NewDawnBank/CuentaBancaria.java
@@ -3,46 +3,88 @@ package NewDawnBank;
 import java.util.ArrayList;
 
 public class CuentaBancaria {
-    private ArrayList<Character> iban;
-    private String titular;
-    private double saldo = 0;
-    public static ArrayList<Integer> movimientos;
 
-    // Constructor para determinar el tamaño de las arrays
-    public CuentaBancaria() {
-        movimientos = new ArrayList<>(100);
-        iban = new ArrayList<>(22);
+    // Atributos constantes (usamos 'final')
+    private final String titular;
+    private final String iban;
+
+    // Atributos variables
+    private double saldo;
+    public static ArrayList<Double> movimientos = new ArrayList<>(100);
+
+    // Cuenta válida
+    private boolean valida;
+
+    public CuentaBancaria(String titular, String iban) {
+        this.titular = titular;
+        this.iban = iban;
+        this.saldo = 0;
+
+        // Comprobamos IBAN y registramos si es cuenta válida
+        if(!iban.matches("^[A-Z]{2}\\d{22}")){
+            System.err.println("ERROR: el formato del IBAN no es correcto");
+            this.valida = false;
+        }
+        else{
+            this.valida = true;
+        }
     }
 
     // Caso 1: Mostrar los datos de la cuenta
-    public String datosCuenta() {
-        return "Titular: " + titular + "\n"
+    public void datosCuenta() {
+        System.out.println("Titular: " + titular + "\n"
                 + "IBAN: " + iban + "\n"
-                + "Saldo: " + saldo + " euros\n";
+                + "Saldo: " + saldo + " euros\n");
     }
 
     // Caso 2: Mostrar el IBAN
-    public String mostrarIban() {
-        return "IBAN: " + iban;
+    public void mostrarIban() {
+        System.out.println("IBAN: " + iban);
     }
 
     // Caso 3: Mostrar el titular
-    public String mostrarTitular() {
-        return "Titular: " + titular;
+    public void mostrarTitular() {
+        System.out.println("Titular: " + titular);
     }
 
     // Caso 4: Mostrar el saldo disponible
-    public String mostrarSaldo() {
-        return "Saldo disponible: " + saldo;
+    public void mostrarSaldo() {
+        System.out.println("Saldo disponible: " + saldo + " euros");
     }
 
     // Caso 5: Ingresar dinero
-    public void ingreso(int cantidad) { // Imprimir si se ha hecho correctamente
-        saldo += cantidad;
-        movimientos.add(cantidad);     // Imprimir también si hay algún error por la cantidad de ingereso o movimientos
+    public void ingresarDinero(double cantidad) { // Imprimir si se ha hecho correctamente
+        if (movimientos.size() == 100) {
+            System.out.println(".|Error, registro de movimientos máximos alcanzados|.");
+        } else {
+            saldo += cantidad;
+            movimientos.add(cantidad); // Imprimir también si hay algún error por la cantidad de ingereso o movimientos
+            System.out.println("Has ingresado " + cantidad + "€ correctamente");
+        }
     }
 
     // Caso 6: Retirada de dinero
+    public void retirarDinero(double cantidad) {
+        saldo -= cantidad; // movimientos.
+        movimientos.add(-cantidad);
+        System.out.println("Has retirado " + cantidad + "€ correctamente");
+        
+
+    }
+
+    // Caso 7: Mostrar movimientos
+    public void mostrarMovimientos() {
+        for (int i = 0; i < movimientos.size(); i++) {
+            System.out.print(movimientos.get(i) + ", ");
+        }
+    }
 
 
+    public boolean isValida() {
+        return valida;
+    }
+
+    public void setValida(boolean valida) {
+        this.valida = valida;
+    }
 }
--- POO Java/NewDawnBank/NewDawnBank.java
+++ POO Java/NewDawnBank/NewDawnBank.java
@@ -1,5 +1,112 @@
 package NewDawnBank;
 
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
 public class NewDawnBank {
+    public static void main(String[] args) {
+        // Borra la terminal para que quede más legible
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+
+        // Mensaje de bienvenida y de introducción de datos
+        System.out.println("Bienvenido a 'New Dawn Bank'");
+        System.out.println("Para acceder a tu cuenta escribe el nombre del titular y el IBAN de la cuenta");
+
+        CuentaBancaria usuario;
+
+
+        do {
+            System.out.println();
+            System.out.print("Titular: ");
+            Scanner scanTitular = new Scanner(System.in);
+            String titular = scanTitular.nextLine();
     
+            System.out.print("IBAN: ");
+            Scanner scanIban = new Scanner(System.in);
+            String iban = scanIban.nextLine();
+
+            usuario = new CuentaBancaria(titular, iban);
+        } while (!usuario.isValida());
+
+
+        // Borra la terminal para que quede más legible
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+
+        boolean isActive = true; // Inicializamos la variable boolean a 'true'
+
+        while (isActive) {
+            // Borra la terminal
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+
+            // Imprime el menú de opciones
+            System.out.println("Elige una opción del 1-8:");
+            System.out.print("1) Mostrar datos de la cuenta\n"
+                    + "2) Mostrar IBAN\n"
+                    + "3) Mostrar titular\n"
+                    + "4) Mostrar saldo \n"
+                    + "5) Ingresar dinero\n"
+                    + "6) Retirar dinero\n"
+                    + "7) Mostrar movimientos\n"
+                    + "8) Salir\n"
+                    + "Elige una opción: ");
+
+            Scanner scanOpcion = new Scanner(System.in);
+            int opcion = scanOpcion.nextInt();
+            System.out.println();
+
+            switch (opcion) {
+                case 1:
+                    usuario.datosCuenta();
+                    break;
+
+                case 2:
+                    usuario.mostrarIban();
+                    break;
+
+                case 3:
+                    usuario.mostrarTitular();
+                    break;
+
+                case 4:
+                    usuario.mostrarSaldo();
+                    break;
+
+                case 5:
+                    System.out.print("Cantidad a ingresar: ");
+                    Scanner scanIngreso = new Scanner(System.in);
+                    int ingreso = scanIngreso.nextInt();
+
+                    usuario.ingresarDinero(ingreso);
+                    break;
+
+                case 6:
+                    System.out.print("Cantidad a retirar: ");
+                    Scanner scanRetirar = new Scanner(System.in);
+                    int retirada = scanRetirar.nextInt();
+
+                    usuario.retirarDinero(retirada);
+                    break;
+
+                case 7:
+                    usuario.mostrarMovimientos();
+                    break;
+
+                case 8:
+                    System.out.println("Gracias por usar tu banco de confianza");
+                    isActive = false;
+                    break;
+                default:
+                    System.out.println("Por favor elige una opción entre 1 y 8");
+            }
+
+            try { // Crea un control de excepciónes para configurar la espera de la terminal
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
